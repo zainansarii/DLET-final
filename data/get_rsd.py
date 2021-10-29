744bc37f64731477a4e9fc592e083f5235631845
Zain Ansari <68613350+zainansarii@users.noreply.github.com>
2021-10-29 15:25:54 +0100

    Update get_rsd.py

--- data/get_rsd.py
+++ data/get_rsd.py
@@ -7,7 +7,7 @@ key = "hn1im51kb8yk3mf"
 time = dt.datetime(2018, 1, 1, 00, 00, 00)
 timedelta = dt.timedelta(minutes=30)
 
-# get 1 year of rolling system demand data from 01/01/2018
+# get 365 days of rolling system demand data from 01/01/2018
 for j in range(365):
   for i in range(48):
       response = requests.get("https://api.bmreports.com/BMRS/ROLSYSDEM/v1?APIKey=" + key + "&FromDateTime=" + str(time) + "&ToDateTime=" + str(time) + "&ServiceType=csv")
